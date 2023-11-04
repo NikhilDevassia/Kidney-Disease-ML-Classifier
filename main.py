d75f0269854602a4f08c2fec003c46f2d1a14cd6
Nikhil Devasia <nikhildevasia2000@gmail.com>
2023-11-04 14:51:10 +0530

    model training completed

--- main.py
+++ main.py
@@ -3,31 +3,31 @@ from KidneyDiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngesti
 from KidneyDiseaseClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from KidneyDiseaseClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 
-# STAGE_NAME = "Data Ingestion stage"
+STAGE_NAME = "Data Ingestion stage"
 
 
-# try:
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#    data_ingestion = DataIngestionTrainingPipeline()
-#    data_ingestion.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
-# STAGE_NAME = "Prepare base model"
+STAGE_NAME = "Prepare base model"
 
 
-# try: 
-#    logger.info(f"*******************")
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#    prepare_base_model = PrepareBaseModelTrainingPipeline()
-#    prepare_base_model.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 STAGE_NAME = "Training"
